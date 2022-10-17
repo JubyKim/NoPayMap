0758da53a23278831b226e4e345b22d1471f7d34
JubyKim <uon9816@naver.com>
2022-10-17 23:01:14 +0900

    get Lat and Long by Keyword

--- NoPayMap/Structs.swift
+++ NoPayMap/Structs.swift
@@ -7,10 +7,14 @@
 
 import Foundation
 
-struct NonPayData {
+struct HospInfo {
+    var id = 0
     var hospName = ""
-    var hospLong = 0
-    var hospLat = 0
     var avgPrice = 0
-    
+}
+
+
+struct HospCoor {
+    var hospLong = 0.0
+    var hospLat = 0.0
 }
--- NoPayMap/ViewController.swift
+++ NoPayMap/ViewController.swift
@@ -15,50 +15,102 @@ import Foundation
 class ViewController: UIViewController, MTMapViewDelegate {
     let mapView = MTMapView()
     
+    var mapItems = [MTMapPOIItem]()
     var avgPrc = 0
     var hospName = ""
     
     let itemCd: String = "ABZ010001"
-    var KEY: String = "M0j%2FyDxQTLEvDEjW3vg0lD4ypKN%2Bo%2Fm7leqrri%2FoXMEBHav1qfUDiEq5DQn0nRBzy3oV48c24mge%2FPPCJtVTTw%3D%3D"
     
+    let OPEN_KEY: String = "M0j%2FyDxQTLEvDEjW3vg0lD4ypKN%2Bo%2Fm7leqrri%2FoXMEBHav1qfUDiEq5DQn0nRBzy3oV48c24mge%2FPPCJtVTTw%3D%3D"
     var xmlParser = XMLParser()
     
     var currentElement = ""                // 현재 Element
     var item:[String:String] = [:]  // item[key] => value
     var elements:[[String:String]] = []
     
-    var results : [NonPayData] = []
-    
+    static var hospInfoList : [HospInfo] = []
+    static var hospCoorList : [HospCoor] = []
     func requestNonPayInfo() {
         // OPEN API 주소
-        let url = "https://apis.data.go.kr/B551182/nonPaymentDamtInfoService/getNonPaymentItemHospList2?serviceKey=\(KEY)" + "&itemCd=\(itemCd)"
+        let url = "https://apis.data.go.kr/B551182/nonPaymentDamtInfoService/getNonPaymentItemHospList2?serviceKey=\(OPEN_KEY)" + "&itemCd=\(itemCd)"
         guard let xmlParser = XMLParser(contentsOf: URL(string: url)!) else { return }
         
         xmlParser.delegate = self;
         xmlParser.parse()
     }
     
+    let headers: HTTPHeaders = [
+                "Authorization": "KakaoAK e9bcbfb89713389a16c296fde3a156fc"
+            ]
+    
+    static var dicValue = NSDictionary();
+    func findAddress(ind: Int) { //-> [Double]
+        print("indindindindind")
+        print(ind)
+//        print("ViewController.hospInfoList)")
+//        print(ViewController.hospInfoList)
+        let parameters: [String: Any] = [
+            "query": ViewController.hospInfoList[ind].hospName,
+            "page": 1,
+            "size": 15
+            ]
+            
+        
+        AF.request("https://dapi.kakao.com/v2/local/search/keyword.json", method: .get,
+             parameters: parameters, headers: headers)
+             .responseJSON(completionHandler: { response in
+                 switch response.result {
+                     
+                 case .success(let value):
+                     print("success")
+                     ViewController.dicValue = value as! NSDictionary
+//                     print("dicValue")
+//                     print(ViewController.dicValue)
+                     ViewController.dicValue = (ViewController.dicValue["documents"] as! Array<Any>)[0] as! NSDictionary
+                     print("dicValue")
+//                     hospCoorList.append(HospCoor(hospLong: <#T##Double#>, hospLat: <#T##Double#>))
+                     print([ViewController.dicValue["x"], ViewController.dicValue["y"]])
+                     
+                case .failure(let error):
+                     print("error 났어요")
+                     print(error)
+                     
+                   }
+                    
+                 
+               })
+        
+//        print("dicValue[x]")
+//        print(dicValue["x"] as! Double)
+//        print("dicValue[y]")
+//        print(dicValue["y"] as! Double)
+//        return
+  
+        }
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
         requestNonPayInfo()
         print("----this is result----")
-        print(results)
-        print("---------------------")
-        findLatLong(hospName: "가톨릭대학교인천성모병원")
+        print(ViewController.hospInfoList)
         
+        print("---------------------")
         self.view.addSubview(mapView)
         mapView.delegate = self
         mapView.baseMapType = .standard
-
         mapView.snp.makeConstraints {
             $0.top.equalToSuperview()
             $0.leading.equalToSuperview()
             $0.trailing.equalToSuperview()
             $0.bottom.equalToSuperview()
             }
+        print("for문 시작")
 
     }
+    
+    
+    
 }
 
 extension ViewController : XMLParserDelegate {
@@ -84,49 +136,42 @@ extension ViewController : XMLParserDelegate {
         
     }
     
+    static var idx = 0;
     func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        
+        
         if elementName == "item" {
-            results.append(NonPayData(hospName: hospName, avgPrice: avgPrc/2))
+            
+            
+            ViewController.hospInfoList.append(HospInfo(id : ViewController.idx,
+                                                     hospName: hospName,
+                                                     avgPrice: avgPrc/2))
+            findAddress(ind:ViewController.idx)
+            ViewController.idx += 1
+            print("parser시작")
+            print(ViewController.hospInfoList)
+            
+//            ViewController.hospCoorList.append(HospCoor(hospLong: findAddress(ind: idx)[1], hospLat: findAddress(ind: idx)[0]))
+            
+            
         }
-    }
-}
+        
+    
+    
 
+    }
+    
+}
 
 extension ViewController {
-    func findLatLong(hospName : String) {
-        let headers: HTTPHeaders = [
-                    "Authorization": KEY
-                ]
-                
-        let parameters: [String: Any] = [
-                    "query": hospName,
-                    "page": 1,
-                    "size": 15
-                ]
-                
-        AF.request("https://dapi.kakao.com/v2/local/search/keyword.json", method: .get,
-             parameters: parameters, headers: headers)
-             .responseJSON(completionHandler: { response in
-                 switch response.result {
-                 case .success(let value):
-                      print(value)
-                   case .failure(let error):
-                       print(error)
-                   }
-               })
+    func poiItem(id: Int, latitude: Double, longitude: Double, imageName: String) -> MTMapPOIItem {
+        let item = MTMapPOIItem()
+        item.tag = id
+        item.mapPoint = MTMapPoint(geoCoord: .init(latitude: latitude, longitude: longitude))
+        item.showAnimationType = .noAnimation
+        return item
     }
+    
+//    mapView.addPOIItem()
+   
 }
-
-
-
-/*
- if let detailsPlace = JSONDecoder(value)["documents"].array{
-     for item in detailsPlace{
-          let placeName = item["place_name"].string ?? ""
-          let roadAdressName = item["road_address_name"].string ?? ""
-          let longitudeX = item["x"].string ?? ""
-          let latitudeY = item["y"].string ?? ""
-          }
-           
-       }
- */
